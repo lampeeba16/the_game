af35f6309b0a510d47edc2d2e1b58202c1dcce82
loesellu15 <luke65@mx.at>
2018-05-02 10:19:40 +0200

    test

--- Reactiongame/main.cpp
+++ Reactiongame/main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <random>
+
+//	test commit
 // LED-PIN - wiringPi-PIN 0 ist BCM_GPIO 17.
 // Wir müssen bei der Initialisierung mit wiringPiSetupSys die BCM-Nummerierung verwenden.
 // Wenn Sie eine andere PIN-Nummer wählen, verwenden Sie die BCM-Nummerierung, und
