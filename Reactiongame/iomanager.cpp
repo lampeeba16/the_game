c6d1354bcadd97905e60679e180b8d7d35752c95
lampeeba16 <marc.lampeebaumgartner@edu.fh-joanneum.at>
2018-05-08 11:16:38 +0200

    sdf

--- Reactiongame/iomanager.cpp
+++ Reactiongame/iomanager.cpp
@@ -1,15 +1,20 @@
 #include "iomanager.h"
+#include <algorithm>
 
 void Io_manager::reserve(int pin)
 {
-
+	reserved_pins_.push_back(pin);
 }
 
 void Io_manager::release(int pin)
 {
+	reserved_pins_.erase(std::remove(reserved_pins_.begin(), reserved_pins_.end(), pin), reserved_pins_.end());//Funktion zum suchen und löschen
 }
 
 bool Io_manager::check(int pin)
 {
-	return false;
+	if (std::find(reserved_pins_.begin(), reserved_pins_.end(), pin) != reserved_pins_.end())//Wenn Element vorhanden     Funktion zum suchen von Element
+		return true;
+	else //wenn nicht vrhandne
+		return false;
 }
